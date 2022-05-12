a8f66d7455e631c51d9256fda6a9f1f30b381adb
Joao Vitor <joaovitorsocial25@gmail.com>
2022-05-12 14:57:23 -0300

    Up Joao

---
+++ Projeto_Java_G2/src/Projeto_Java_G2/Teste_01.java
@@ -0,0 +1,11 @@
+package Projeto_Java_G2;
+
+public class Teste_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int x;
+			System.out.println("Aooba");
+	}
+
+}
